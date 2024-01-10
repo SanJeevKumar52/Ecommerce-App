14fbc8abd846f445fd2c7ca12309c8eb7505e230
Sanjeev <sk3616593@gmail.com>
2024-01-11 01:48:25 +0530

    SignIn page added

--- src/pages/SignInpage/SignInpage.js
+++ src/pages/SignInpage/SignInpage.js
@@ -1,13 +1,22 @@
+// SignInpage.js
+import React from "react";
 import { NavLink } from "react-router-dom";
 import styles from './signinpage.module.css';
+
 const SignInpage = () => {
   return (
-    <>
-      <div className="signinPageContainer" >
-
-      </div>
-    </>
-  )
+    <div className={styles.signInPageContainer}>
+      <form className={styles.signInform}>
+        <h2>Sign In</h2>
+        <input type="email" id="email" name="email" placeholder="Enter Email" required />
+        <input type="password" id="password" name="password" placeholder="Enter Password" required />
+        <button type="submit">Sign In</button>
+        <p>
+          Don't have an account? <NavLink to="/signup">Sign Up</NavLink>
+        </p>
+      </form>
+    </div>
+  );
 }
 
-export default SignInpage
+export default SignInpage;
